c18e3938f9b60fe029eb577483d161268816899b
Jorge <newdarkjg@gmail.com>
2024-03-21 13:31:11 +0100

    Actualizacion

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -4,32 +4,57 @@ import { fetchCoinByID } from '../utils/coins';
 import styles from "./Coin.module.css"
 
 function Coin() {
+
+    console.log((localStorage.getItem("favorites")));
     const [coin, setCoin] = useState();
+    const [favs, setFavorites] = useState([])
     let { id } = useParams();
 
+
     useEffect(() => {
-        const getData = async() => {
+        const getData = async () => {
             const data = await fetchCoinByID(id)
             setCoin(data.data)
             console.log(data.data)
         }
         getData()
+
     }, [])
 
     const addToFavorite = () => {
-        localStorage.setItem("favorites",coin.id)
+        console.log(JSON.parse(localStorage.getItem("favorites"))+"Log")
+        console.log(favs + "favs")
+        setFavorites([
+            ...favs,
+            JSON.parse(localStorage.getItem("favorites"))
+        ])
+        console.log(favs + "favs")
+        setFavorites([
+            ...favs,
+            coin.id
+        ])
+        setFavorites(Oldfavs => {
+            return Oldfavs.filter(favsold => favsold != null)
+        })
+        setFavorites(Oldfavs => {
+            return Oldfavs.filter((favsold, i) => Oldfavs.indexOf(favsold) == i)
+        })
+
+        localStorage.setItem("favorites", JSON.stringify(favs))
+       
+
     }
 
     return (
         <>
-        {coin ? (
-            <div>
-                <h2>{coin.name}</h2>
-                <button onClick={addToFavorite}>Añadir a favoritos</button>
-            </div>
-        ) : (
-            'Loading'
-        )}
+            {coin ? (
+                <div>
+                    <h2>{coin.name}</h2>
+                    <button onClick={addToFavorite}>Añadir a favoritos</button>
+                </div>
+            ) : (
+                'Loading'
+            )}
 
         </>
 
--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 import { fetchCoinByID } from '../utils/coins';
-
+import { Link } from 'react-router-dom'
 function Favorites() {
   const [IDs, setIDs] = useState([]);
   const [vacio, setVacio] = useState(null);
-  const [favorites, setFavorites] = ([])
+  const [favorites, setFavorites] = useState([])
+  const [datosId, setDatosId] = useState("")
   useEffect(() => {
 
-    const favo = (localStorage.getItem("favorites"))
+    const favo = (JSON.parse(localStorage.getItem("favorites")))
 
     console.log(favo)
     if (favo != null) {
@@ -20,22 +21,31 @@ function Favorites() {
     } else {
       setVacio("No existen favoritos")
     }
-  })
+  },[])
   async function getData(id) {
     const data = await fetchCoinByID(id)
+    console.log(data)
     return data
   }
   return (
-    IDs.map((id) => {
-      { let datosId = getData(id) }
-      <Link key={datosId.id} to={`/coin/${datosId.id}`}>
-        <li>
-          <h2 className={styles.title}>{datosId.name}</h2>
-          <p>{datosId.symbol}</p>
-          <p>{datosId.priceUsd}</p>
-        </li>
-      </Link>
-    })
+    <>
+      <ul>
+        {IDs.map((id) => {
+         { setDatosId(getData(id))}
+            < li >
+            <Link key={datosId.id} to={`/coin/${datosId.id}`}>
+              <li>
+                <h2 >{datosId.name}</h2>
+                <p>{datosId.symbol}</p>
+                <p>{datosId.priceUsd}</p>
+              </li>
+            </Link>
+            </li>
+
+
+        })}
+    </ul >
+    </>
   )
 }
 
